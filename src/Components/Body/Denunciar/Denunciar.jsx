20f3cc9fbd0edaf82884e5dc2b4d7753a92e3059
RudeusGrayrat <miguelnc2502@gmail.com>
2026-03-10 20:03:53 -0500

    cambio de icono

--- src/Components/Body/Denunciar/Denunciar.jsx
+++ src/Components/Body/Denunciar/Denunciar.jsx
@@ -11,9 +11,9 @@ const Denunciar = () => {
                 style={{ borderColor: "rgba(250, 240, 55, 0.28)", boxShadow: "0 5px 8px 6px rgba(103, 98, 0, 0.25)" }}
                 onClick={() => setFormDenunciaShow(true)}
             >
-                <img src="/SUBIR.svg" alt="SUBIR" className="h-[70%]" />
-                <span className="mt-5 text-3xl max-md:text-xl font-medium text-black">
-                    Subir denuncia
+                <img src="/SILBATO_BLACK.svg" alt="SUBIR" className="h-[70%]" />
+                <span className="m-3 mt-10 text-3xl max-md:text-xl font-medium text-black">
+                    Empezar denuncia
                 </span>
             </div>
 
